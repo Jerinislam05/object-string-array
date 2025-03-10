46bea7e114052f73345e7023303e536c5d8bfb23
Jerin Islam <jerinislam05@gmail.com>
2025-03-10 08:54:51 +0000

    unit convert with function

---
+++ convert.js
@@ -0,0 +1,31 @@
+// 12 inch = 1 feet
+function inchToFeet(inch) {
+    const feet = inch / 12;
+    return feet;
+}
+// another way
+function inchToFeet2(inch) {
+    const feetFraction = inch / 12;
+    const feetNumber = parseInt(feetFraction);
+    const inchRemaining = inch % 12;
+    const result = feetNumber + " ft " + inchRemaining + " inch."
+    return result;
+}
+const suvoHeight = inchToFeet(75);
+// console.log(suvoHeight);
+const suvoHeight2 = inchToFeet2(75);
+// console.log(suvoHeight2);
+
+// miles To kilometers
+function mileToKm(mile) {
+    const km = mile * 1.60934;
+    return km;
+}
+const khulnaToDhaka = mileToKm(400);
+console.log(khulnaToDhaka);
+function kmToMiles(km) {
+    const mile = km * 0.621371;
+    return mile;
+}
+const dhakaToKhulna = kmToMiles(400);
+console.log(dhakaToKhulna);
